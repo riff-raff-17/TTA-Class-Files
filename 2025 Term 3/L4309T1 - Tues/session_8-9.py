6b0ea421220557b7166d71940cabd20069cead64
riff-raff-17 <65214797+riff-raff-17@users.noreply.github.com>
2025-08-26 17:41:10 +0800

    Create session_8-9.py

---
+++ 2025 Term 3/L4309T1 - Tues/session_8-9.py
@@ -0,0 +1,93 @@
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+from PIL import Image
+
+app = Ursina()
+
+MC_RGB = [
+    (112, 192, 87), # grass green
+    (183, 157, 97), # grass brown
+    (47, 242, 255) # ice blue
+]
+
+def make_dirt(path='dirt.png', size=16):
+    img = Image.new("RGB", (size, size))
+    p = img.load()
+    base = (159, 104, 62)
+
+    for y in range(size):
+        for x in range(size):
+            # Add some per pixel noise so they don't all look the same
+            n = random.randint(-14, 14)
+            r = max(0, min(255, base[0] + n))
+            g = max(0, min(255, base[1] + n))
+            b = max(0, min(255, base[2] + n))
+
+            # Occasional dark
+            if random.random() < 0.06:
+                r, g, b = int(r*0.7), int(g*0.7), int(b*0.7)
+
+            # Occasional light
+            if random.random() < 0.05:
+                r, g, b = min(255, int(r*1.15)),min(255, int(g*1.15)), min(255, int(b*1.15))
+
+            p[x, y] = (r, g, b)
+        img.save(path)
+        return path
+    
+def make_grass(path='grass.png', size=16):
+    img = Image.new('RGB', (size, size))
+    p = img.load()
+    base=(95, 159, 53)
+    for y in range(size):
+        for x in range(size):
+            n = random.randint(-10, 10)
+            r = max(0, min(255, base[0] + n))
+            g = max(0, min(255, base[1] + n))
+            b = max(0, min(255, base[2] + n))
+            if random.random() < 0.03:
+                r, g, b = min(255,int(r*1.1)), min(255,int(g*1.1)), min(255,int(b*1.05))
+            p[x, y] = (r, g, b)
+    img.save(path)
+    return path
+
+DIRT_PATH = 'dirt.png'
+GRASS_PATH = 'grass.png'
+if not os.path.exists(DIRT_PATH):
+    make_dirt(DIRT_PATH, size=16)
+if not os.path.exists(GRASS_PATH):
+    make_grass(GRASS_PATH, size=16)
+
+tex_dirt = load_texture(DIRT_PATH)
+tex_grass = load_texture(GRASS_PATH)
+
+class Voxel(Button):
+    def __init__(self, position=(0, 0, 0)):
+        super().__init__(
+            parent=scene,
+            position=position,
+            origin_y=0.5,
+            model=None,
+            collider='box',
+            highlight_color=color.lime
+        )
+
+        # Dirt base (full cube)
+        self.base = Entity(
+            parent=self,
+            model='cube',
+            texture=tex_dirt,
+            color=color.white,
+            scale=(1, 1, 1),
+            y=0
+        )
+
+        cap_height = 0.04
+        self.cap = Entity(
+            parent=self,
+            model='cube',
+            texture=tex_grass,
+            color=color.white,
+            scale=(1.001, cap_height, 1.001),
+        )
+
