153639d07c17f9bf30af45cd8cd097a83aea6e87
Rafa <65214797+riff-raff-17@users.noreply.github.com>
2026-04-26 09:23:26 +0800

    Create session_12.py

---
+++ External Classes/NTU/session_12.py
@@ -0,0 +1,5 @@
+import cv2
+import mediapipe as mp
+from mediapipe.tasks import python as mp_python
+from mediapipe.tasks.python import vision
+from mediapipe.tasks.python.components.containers.landmark import NormalizedLandmark
