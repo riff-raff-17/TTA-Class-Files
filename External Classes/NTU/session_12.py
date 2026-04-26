cae2b344428b11578e9f826d8f866c513b3ddc8e
Rafa <65214797+riff-raff-17@users.noreply.github.com>
2026-04-26 09:23:45 +0800

    Update session_12.py

--- External Classes/NTU/session_12.py
+++ External Classes/NTU/session_12.py
@@ -3,3 +3,12 @@ import mediapipe as mp
 from mediapipe.tasks import python as mp_python
 from mediapipe.tasks.python import vision
 from mediapipe.tasks.python.components.containers.landmark import NormalizedLandmark
+import urllib.request
+import os
+
+# Download the hand landmark model if not present
+MODEL_PATH = "hand_landmarker.task"
+MODEL_URL = "https://storage.googleapis.com/mediapipe-models/hand_landmarker/hand_landmarker/float16/1/hand_landmarker.task"
+
+# https://pastebin.com/91p9sGXV
+# tinyurl.com/mediapipes2
