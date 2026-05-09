f848162c3025a022765a89711ed2bd485d58f431
Rafa <65214797+riff-raff-17@users.noreply.github.com>
2026-05-09 11:06:47 +0800

    Create session2b.py

---
+++ External Classes/Bendemeer Sec/session2b.py
@@ -0,0 +1,13 @@
+import turtle
+import math
+
+t = turtle.Turtle()
+t.speed(0) # Fastest speed
+t.width(2)
+
+colors = ["red", "orange", "yellow", "green", "cyan", "blue", 
+          "violet", "magenta"]
+
+num_rings = 8
+petals = 12 # Number of circles per ring
+
